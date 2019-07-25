98cd2c6aaf7664eebec575553bbbc184f27d5d91
huanglibing <791554795@qq.com>
2019-07-25 00:33:32 -0700

    gps GGA 解析

--- src/serial_Port/src/listener.cpp
+++ src/serial_Port/src/listener.cpp
@@ -4,7 +4,7 @@
 #include <iomanip>
 void chatterCallback(const serial_Port::GPSConstPtr& msg)
 {
-    std::cout << std::setiosflags(std::ios::fixed) << std::setprecision(7) << "纬度：" << msg->lat << " 经度：" << msg->lon << "\n";
+    std::cout << std::setiosflags(std::ios::fixed) << std::setprecision(7) << "纬度：" << msg->lat << " 经度：" << msg->lon << " 定位状态：" << msg->gpsStatue << " 卫星：" << msg->gpsNum << " 水平精度：" << msg->gpsHdop << "\n";
 }
 
 int main(int argc, char **argv)
--- src/serial_Port/src/serialPort.cpp
+++ src/serial_Port/src/serialPort.cpp
@@ -8,41 +8,180 @@
 #include <cmath>
 #include <cstdlib>//string转化为double
 #include <iomanip>//保留有效小数
+#include <iostream>
 #include "serial_Port/GPS.h"
 serial::Serial ser; //声明串口对象
+serial_Port::GPS GPS_data;//全局变量，解析后数据
+int debug_break[10];
+float debug_break_float[10];
 
-//解析GPS
-void RecePro(std::string s , double& lat , double& lon )
+
+const unsigned char  HEX_TO_ASCII[16] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+
+/**********************************************************************************************
+函数名称: unsigned char  CheckGpsData( unsigned char * Addr )
+功    能: 校验GPS数据
+输    入: null
+输    出: null
+日    期：7.8
+作    者：
+**********************************************************************************************/
+unsigned char  CheckGpsData( unsigned char * Addr )
 {
-    //分割有效数据，存入vector中
-    std::vector<std::string> v;
-    std::string::size_type pos1, pos2;
-    pos2 = s.find(",");
-    pos1 = 0;
-    while ( std::string::npos !=pos2 )
-    {
-        v.push_back( s.substr( pos1, pos2-pos1 ) );
-        pos1 = pos2 + 1;
-        pos2 = s.find(",",pos1);
-    }
-    if ( pos1 != s.length() )
-        v.push_back( s.substr( pos1 ));
-    //解析出经纬度
-    if (v.max_size() >= 6 && (v[6] == "1" || v[6] == "2" || v[6] == "3" || v[6] == "4" || v[6] == "5" || v[6] == "6" || v[6] == "8" || v[6] == "9"))
-    {
-        //纬度
-        if (v[2] != "") lat = std::atof(v[2].c_str()) / 100;
-        int ilat = (int)floor(lat) % 100;
-        lat = ilat + (lat - ilat) * 100 / 60;
-        //经度
-        if (v[4] != "") lon = std::atof(v[4].c_str()) / 100;
-        int ilon = (int)floor(lon) % 1000;
-        lon = ilon + (lon - ilon) * 100 / 60;
+    unsigned char  crc8 = 0;
 
+    while( *Addr != '$' )
+    {
+        if( ( *Addr == '\0' ) || ( *Addr == '*' ) ) return 0;
+
+        Addr++;
     }
+
+    Addr++;
+    crc8 = *Addr++;
+
+    while( *Addr != '*' )
+    {
+        if( *Addr == '\0' ) return 0;
+
+        crc8 ^= *Addr++;
+    }
+
+    Addr++;
+
+    if( HEX_TO_ASCII[( crc8 >> 4 ) & 0x0f] == *Addr++ )
+    {
+        if( HEX_TO_ASCII[crc8 & 0x0f] == *Addr )
+        {
+            return 1;
+        }
+    }
+
+    return 0;
 }
+
+
+
+/**********************************************************************************************
+函数名称: RecePro(std::string s,int len)
+功    能: 解析接收到的数据，//提取GGA,RMC中数据
+输    入: 数据缓存指针，数据长度
+输    出: null
+日    期：8.2
+作    者：
+**********************************************************************************************/
+void RecePro(std::string s,int len)
+{
+    	int start = -1, commanum = 0, BufIndex = 0 ,i = 0;  //临时变量     
+	std::string str;		//临时变量，一帧数据
+	std::string   gpsNum;	 	//星数
+	std::string   gpsStatue ;	//定位状态
+	std::string   gpsHdop;    	//海拔高度 
+    	std::string   LatitudeBuf;	//纬度
+    	std::string   LongitudeBuf;	//经度 
+    	std::string   AltitudeBuf;    	//海拔高度 
+	std::string   temp_str; 	//临时变量
+
+
+    	/*******************************************************GGA语句解析部分**************************************************************/
+	start = s.find("GGA"); //从缓存里找到GGA开头数据，字符串所在地址
+	debug_break[3]=start;	
+	if(start>0)	//找到字符串开头
+	{
+		str=s.substr(start);
+		if(1)	//CRC校验，校验成功
+		{
+			//解析GGA数据，提取需要的参数
+
+		    commanum = 0;
+		    i = 0;
+		    temp_str.clear();
+
+		    /************************解析GGA数据，提取需要的参数*******************/
+		    do
+		    {
+		        if( ( str[i++] ) == ',' )
+		        {
+		            commanum++;
+		            BufIndex = 0;
+		        }
+
+		        if( str[i] != ',' ) //"$GNGGA,122020.70,3908.17943107,N,11715.45190423,E,1,17,1.5,19.497,M,-8.620,M,,*54\r\n";
+		        {
+		            switch( commanum )
+		            {
+		                case 2://纬度
+				    if( BufIndex < ( 17) )
+				    {
+                                	temp_str = str.substr(i,1);
+					LatitudeBuf+=temp_str;
+				    }				    
+		                    break;
+		                case 4://经度
+				    if( BufIndex < ( 17) )
+				    {
+                                	temp_str = str.substr(i,1);
+					LongitudeBuf+=temp_str;
+				    }				    
+		                    break;
+		                case 6://状态
+				    if( BufIndex < ( 2) )
+				    {
+                                	temp_str = str.substr(i,1);
+					gpsStatue+=temp_str;
+				    }
+		                    break;
+		                case 7://卫星数 
+				    if( BufIndex < ( 2) )
+				    {
+                                	temp_str = str.substr(i,1);
+					gpsNum+=temp_str;
+				    }
+		                    break;
+		                case 8://水平精度因子
+				    if( BufIndex < ( 5) )
+				    {
+                                	temp_str = str.substr(i,1);
+					gpsHdop+=temp_str;
+				    }
+		                    break;
+		                default:
+		                    break;
+		            }
+
+		            if( commanum > 14 )
+		                break;
+
+		            BufIndex++;
+		        }
+		    }
+		    while( str[i] != '*' );
+
+		 std::cout << "lat:" << LatitudeBuf<< " lon:" << LongitudeBuf << " S:" << gpsStatue << " num:" << gpsNum << " hdop:" << gpsHdop << "\r\n";
+
+          		/****************************解析GGA数据完成******************************/
+			if(commanum==14)
+			{
+				GPS_data.lat = atof(LatitudeBuf.c_str())/100;
+				GPS_data.lon = atof(LongitudeBuf.c_str())/100;
+				GPS_data.gpsStatue = atoi(gpsStatue.c_str());
+				GPS_data.gpsNum = atoi(gpsNum.c_str());
+				GPS_data.gpsHdop = atof(gpsHdop.c_str());
+			}
+		}
+	}
+
+
+}
+
+//主函数
 int main(int argc, char** argv)
 {
+	static int len;
+	static int len_total;
+	static int first_receive_data=0;
+	
+
     //初始化节点
     ros::init(argc, argv, "serial_node");
     //声明节点句柄
@@ -72,26 +211,79 @@ int main(int argc, char** argv)
         return -1;
     }
 
-    //设置循环的频率 20HZ
-    ros::Rate loop_rate(50);
+    //设置循环的频率 20HZ 50ms 要求循环频率大于数据接收频率
+    ros::Rate loop_rate(20);
 
     std::string strRece;
     while (ros::ok())
     {
+	//获取数据长度
+	len = ser.available();
+	len_total += len;       
 
-        if (ser.available())
+	if (len>0)	//接收数据
         {
-            //1.读取串口信息：
-            //ROS_INFO_STREAM("Reading from serial port\n");
+		//断点分析
+		debug_break[0]++;
 
-            //通过ROS串口对象读取串口信息
-            strRece += ser.read(ser.available());
-            std::cout << strRece ;
-
-
+		//接收数据，标志位置一
+		first_receive_data=1;
+			
+            	//通过ROS串口对象读取串口信息
+            	strRece += ser.read(ser.available());
+            	//std::cout << strRece ;
+	    
         }
-    ros::spinOnce();
-    loop_rate.sleep();
+	else	//处理数据
+	{
+		
+		if(first_receive_data==1)	//第一次则处理收到数据
+		{
+			//断点分析
+			debug_break[1]++;
+
+			//模拟数据
+			//strRece = "$GNGGA,122020.70,3908.179431078,N,11715.45190423,E,1,17,1.5,19.497,M,-8.620,M,,*54\r\n";
+			len_total=strRece.length();
+	
+			//数据处理
+			RecePro(strRece,len_total);
+	
+			//缓冲区清零
+			strRece.clear();
+
+			//发布话题消息
+			GPS_pub.publish(GPS_data);
+
+			//清除标志位
+			first_receive_data=0;
+			len_total=0;
+			
+		}
+		else	//数据处理完成，等待
+		{
+			//断点分析
+			debug_break[2]++;	
+			
+			//输出数据	
+			//std::cout << "test data:" << len << "\r\n";
+		}
+	}
+
+	//断点数据分析，后期待删除
+	static int debug_100ms=0;
+	debug_100ms++;
+	if(debug_100ms >= 2)
+	{
+		//std::cout << "b1:" << debug_break[0]<< " b2:" << debug_break[1] << " b3:" << debug_break[2]  << "\r\n";	
+		//std::cout << "f1:" << debug_break_float[0]<< " f2:" << debug_break_float[1] << " f3:" << debug_break_float[2]  << "\r\n";
+		debug_100ms=0;	
+	}
+
+		
+	ros::spinOnce();
+	loop_rate.sleep();
     }
 }
+
 
